46be4d6ceacfdd68b64a32c287f5c79b5e119fb4
Sufyan <khotsufyan@gmail.com>
2022-07-21 12:15:29 +0530

    Added respository definition text

--- demo/src/main/java/com/allaboutspring/demo/jpa/UserRepository.java
+++ demo/src/main/java/com/allaboutspring/demo/jpa/UserRepository.java
@@ -10,4 +10,9 @@ public interface UserRepository extends CrudRepository<UserEntity, Integer>{
 
 /*
 Once we extend CrudRepository we get some methods such as findById(), findAllById(), delete(), save()
+
+An alternative way is to use @RepositoryDefinition and then we dont have to extend the CrudRepository
+
+We get methods that follow the below convention
+find(First[count])By[property expression][comparison operator][ordering operator]
 */
